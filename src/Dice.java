c7fad527bd74ea2bb045aed1b04f0d27c494f450
Wytze Jan Riedstra <wytzejan.riedstra@duo.nl>
2019-01-21 10:14:01 +0100

    Topic: Weekopdracht Dice Rolling DevOps

---
+++ src/Dice.java
@@ -0,0 +1,16 @@
+public class Dice {
+    public static void main(String[] args) {
+        int totalSum = 0;
+        for (int i = 1; i <= 3; i++) {
+            totalSum += roll();
+        }
+        System.out.println("Total sum of the dices is: " + totalSum);
+    }
+
+    private static int roll() {
+        int range = (6 - 1) + 1; // (max - min) + min
+        int randomNumber = (int) ((Math.random() * range) + 1);
+        System.out.println(randomNumber);
+        return randomNumber;
+    }
+}
