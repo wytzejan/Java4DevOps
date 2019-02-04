1534daf49e844ca7e935526f887fa67ee2c83faf
Wytze Jan Riedstra <wytzejan.riedstra@duo.nl>
2019-02-04 13:36:39 +0100

    Continue with Weekopdracht II Kermisattractie

--- src/apps/kermisattractie/KermisattractieApp.java
+++ src/apps/kermisattractie/KermisattractieApp.java
@@ -2,22 +2,47 @@ package apps.kermisattractie;
 
 import model.kermisattractie.Kermisattractie;
 
+import java.text.DecimalFormat;
+
 public class KermisattractieApp {
+    static Kermisattractie[] kermisattracties = {
+            new Kermisattractie(8.00, "Achtbaan", 2),
+            new Kermisattractie(4.50, "Botsauto's", 20),
+            new Kermisattractie(3.00, "Draaimolen", 15),
+            new Kermisattractie(1.50, "Glijbaan", 40),
+            new Kermisattractie(2.50, "Reuzenrad", 35),
+            new Kermisattractie(5.00, "Schommelschip", 20),
+            new Kermisattractie(5.50, "Spookhuis", 15),
+            new Kermisattractie(1.00, "Botsauto's", 50),
+            new Kermisattractie(6.50, "Wildwaterbaan", 5),
+            new Kermisattractie(2.00, "Zweefmolen", 15)
+    };
 
-    Kermisattractie kermisattracties = new Kermisattractie();
-    kermisattracties[0] = new Kermisattractie(10.00, "Achtbaan", 10);
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        Double omzet = 0.00;
+        Double totaleOmzet = 0.00;
+        for (Kermisattractie kermisattractie : kermisattracties) {
+            System.out.println(kermisattractie.getName() + " is running...!");
+            totaleOmzet += perform(kermisattractie, omzet);
+            System.out.println(" ");
+        }
+        System.out.println("De totale omzet is €" + df.format(totaleOmzet));
+        Double totalTax = totaleOmzet * 0.21;
+        System.out.println("Total tax is €" + df.format(totalTax));
+    }
 
-    Kermisattractie achtbaan = new Kermisattractie(10.00, "Achtbaan", 10);
-    Kermisattractie botsautos = new Kermisattractie(4.50, "Botsauto's", 20);
-    Kermisattractie draaimolen = new Kermisattractie(3.00, "Draaimolen", 15);
-    Kermisattractie glijbaan = new Kermisattractie(1.50, "Glijbaan", 40);
-    Kermisattractie reuzenrad = new Kermisattractie(2.50, "Reuzenrad", 35);
-    Kermisattractie schommelschip = new Kermisattractie(5.00, "Schommelschip", 20);
-    Kermisattractie spookhuis = new Kermisattractie(5.50, "Spookhuis", 15);
-    Kermisattractie springkussen = new Kermisattractie(1.00, "Botsauto's", 50);
-    Kermisattractie wildwaterbaan = new Kermisattractie(6.50, "Wildwaterbaan", 5);
-    Kermisattractie zweefmolen = new Kermisattractie(2.00, "Zweefmolen", 15);
-
-    private static void perform() {
+    private static double perform(Kermisattractie kermisattractie, Double omzet) {
+        for (int i = 1; i <= 3; i++) {
+            kermisattractie.setRounds(i);
+            omzet += kermisattractie.getPrice();
+            System.out.println("Round " + i);
+            if (kermisattractie.getRounds().equals(kermisattractie.getRoundsBeforeInspection())) {
+                System.out.println("WARNING! "+ kermisattractie.getName() + " must be inspected! No further rounds can be made.");
+                break;
+            }
+        }
+        System.out.println("Totale omzet: " + omzet);
+        return omzet;
     }
 }
