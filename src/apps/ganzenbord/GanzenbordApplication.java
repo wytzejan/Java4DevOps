fb9ab703a7ddf895d9d8c1bf0df44bc1df484cce
Wytze Jan <10392890+wytzejan@users.noreply.github.com>
2019-02-12 13:28:02 +0100

    Add files via upload

--- src/apps/ganzenbord/GanzenbordApplication.java
+++ src/apps/ganzenbord/GanzenbordApplication.java
@@ -11,14 +11,14 @@ public class GanzenbordApplication {
         System.out.println("--------------------------");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Met hoeveel personen wil je het spel spelen? (antwoordmogelijkheden: 1 en 2)");
+        System.out.println("Met hoeveel personen wil je het spel spelen?");
         int numberOfPlayers = scanner.nextInt();
         System.out.println("Je speelt het spel met " + numberOfPlayers + " speler(s).");
 
         Player[] players = new Player[numberOfPlayers];
 
         for (int i = 0; i < players.length; i++) {
-            int playerNumber = i + 1;
+            int playerNumber = i + 1;   
             scanner = new Scanner(System.in);
             System.out.println("Naam speler " + playerNumber + "?");
             String playerName = scanner.nextLine();
@@ -41,6 +41,7 @@ public class GanzenbordApplication {
         boolean endGame = false;
         int brugNumber = 6;
         int herbergNumber = 19;
+        int putNumber = 31;
         int doolhofNumber = 42;
         int inJail = 52;
         int doodNumber = 58;
@@ -48,9 +49,14 @@ public class GanzenbordApplication {
         List<Integer> backToStart = Arrays.asList(25, 45);
         List<Integer> bonusPlaces = Arrays.asList(10, 20, 30, 40, 50, 60);
 
+        outer:
         do {
             for (Player player : players) {
-                if (!player.isActivePlayer()) {
+                if (endGame) {
+                    break outer;
+                }
+                else if (!player.isActivePlayer() && !endGame) {
+
                     /* logic for skipTurn */
                     if (player.isSkipTurn() && player.getSkipNumberOfTurns() > 0) {
                         int tempSkipNumberOfTurns = player.getSkipNumberOfTurns();
@@ -58,61 +64,66 @@ public class GanzenbordApplication {
                         if (player.getSkipNumberOfTurns() == 0) {
                             player.setSkipTurn(false);
                         }
+                    } else if (player.isInPut()) {
+                        continue;
                     } else {
 
                         System.out.println(player.getName() + " is aan de beurt. Gooi je dobbelsteen (g).");
                         do {
-                            scanner = new Scanner(System.in);
-                            String text = scanner.nextLine();
-                            if (text.equals("g")) {
-                                int thrownNumber = throwDice();
-                                int currentPlace = player.getCurrentPlace();
-                                player.setCurrentPlace(currentPlace += thrownNumber);
+                            int thrownNumber = throwDice();
+                            int currentPlace = player.getCurrentPlace();
+                            player.setCurrentPlace(currentPlace += thrownNumber);
 
-                                /* special place: 10, 20, 30, 40, 50 en 60 houdt in dat je het geworpen aantal nog een keer loopt. */
-                                if (bonusPlaces.contains(player.getCurrentPlace())) {
-                                    System.out.println("Je hebt " + thrownNumber + " gegooid, je staat op plaats " + player.getCurrentPlace() + ".");
-                                    int tempCurrentPlace = player.getCurrentPlace();
-                                    player.setCurrentPlace(tempCurrentPlace += thrownNumber);
-                                    System.out.println("Yeah! Dit is een bonusplaats! Je bent nog eens " + thrownNumber + " vooruit gegaan en staat nu op " + player.getCurrentPlace() + ".");
-                                    endGame = currentPlaceAboveFinish(endGame, finish, player, thrownNumber);
+                            /* special place: 10, 20, 30, 40, 50 en 60 houdt in dat je het geworpen aantal nog een keer loopt. */
+                            if (bonusPlaces.contains(player.getCurrentPlace())) {
+                                System.out.println("Je hebt " + thrownNumber + " gegooid, je staat op plaats " + player.getCurrentPlace() + ".");
+                                int tempCurrentPlace = player.getCurrentPlace();
+                                player.setCurrentPlace(tempCurrentPlace += thrownNumber);
+                                System.out.println("Yeah! Dit is een bonusplaats! Je bent nog eens " + thrownNumber + " vooruit gegaan en staat nu op " + player.getCurrentPlace() + ".");
+                                if (player.getCurrentPlace() == putNumber) {
+                                    inPutLogica(players, player);
+                                }
+                                endGame = currentPlaceAboveFinish(endGame, finish, player, thrownNumber);
+                            }
 
-                                }
+                            /* special place: 25 en 45 is terug naar start. */
+                            else if (backToStart.contains(player.getCurrentPlace())) {
+                                System.out.println("Helaas! Je staat op " + player.getCurrentPlace() + ". Je moet terug naar start.");
+                                player.setCurrentPlace(0);
+                            }
 
-                                /* special place: 25 en 45 is terug naar start. */
-                                else if (backToStart.contains(player.getCurrentPlace())) {
-                                    System.out.println("Helaas! Je staat op " + player.getCurrentPlace() + ". Je moet terug naar start.");
-                                    player.setCurrentPlace(0);
-                                }
+                            /* special price: 31, put, Wie hier komt moet er blijven tot een andere speler er komt. Degene die er het eerst was speelt dan verder. */
+                            else if (player.getCurrentPlace() == putNumber) {
+                                inPutLogica(players, player);
+                            }
 
-                                /* special place: 52, gevangenis, drie beurten overslaan */
-                                else if (player.getCurrentPlace() == inJail) {
-                                    System.out.println("Helaas! Je moet 3 beurten overslaan.");
-                                    player.setSkipTurn(true);
-                                    player.setSkipNumberOfTurns(3);
-                                } else if (player.getCurrentPlace() == brugNumber) {
-                                    System.out.println("Yeah! De brug. Je gaat verder naar 12!");
-                                    player.setCurrentPlace(12);
-                                } else if (player.getCurrentPlace() == doolhofNumber) {
-                                    System.out.println("Helaas! Je bent in het doolhof. Je moet terug naar 39.");
-                                    player.setCurrentPlace(39);
-                                } else if (player.getCurrentPlace() == doodNumber) {
-                                    System.out.println("Helaas! Je bent dood. Je gaat terug naar start.");
-                                    player.setCurrentPlace(0);
-                                }
-                                /* 19, herberg, Een beurt overslaan */
-                                else if (player.getCurrentPlace() == herbergNumber) {
-                                    System.out.println("Helaas! Je moet een beurt overslaan.");
-                                    player.setSkipTurn(true);
-                                    player.setSkipNumberOfTurns(1);
-                                }
+                            /* special place: 52, gevangenis, drie beurten overslaan */
+                            else if (player.getCurrentPlace() == inJail) {
+                                System.out.println("Helaas! Je moet 3 beurten overslaan.");
+                                player.setSkipTurn(true);
+                                player.setSkipNumberOfTurns(3);
+                            } else if (player.getCurrentPlace() == brugNumber) {
+                                System.out.println("Yeah! De brug. Je gaat verder naar 12!");
+                                player.setCurrentPlace(12);
+                            } else if (player.getCurrentPlace() == doolhofNumber) {
+                                System.out.println("Helaas! Je bent in het doolhof. Je moet terug naar 39.");
+                                player.setCurrentPlace(39);
+                            } else if (player.getCurrentPlace() == doodNumber) {
+                                System.out.println("Helaas! Je bent dood. Je gaat terug naar start.");
+                                player.setCurrentPlace(0);
+                            }
+                            /* 19, herberg, Een beurt overslaan */
+                            else if (player.getCurrentPlace() == herbergNumber) {
+                                System.out.println("Helaas! Je moet een beurt overslaan.");
+                                player.setSkipTurn(true);
+                                player.setSkipNumberOfTurns(1);
+                            }
 
-                                /* finish */
-                                else if (player.getCurrentPlace() >= finish) {
-                                    currentPlaceAboveFinish(endGame, finish, player, thrownNumber);
-                                } else {
-                                    System.out.println("Je hebt " + thrownNumber + " gegooid, je staat op plaats " + player.getCurrentPlace() + ", niks aan de hand.");
-                                }
+                            /* finish */
+                            else if (player.getCurrentPlace() >= finish) {
+                                endGame = currentPlaceAboveFinish(endGame, finish, player, thrownNumber);
+                            } else {
+                                System.out.println("Je hebt " + thrownNumber + " gegooid, je staat op plaats " + player.getCurrentPlace() + ", niks aan de hand.");
                             }
                             player.setActivePlayer(false);
                             System.out.println("--------------------------");
@@ -128,6 +139,18 @@ public class GanzenbordApplication {
         } while (!endGame);
     }
 
+    private static void inPutLogica(Player[] players, Player player) {
+        for (Player element : players) {
+            if (element.isInPut() && (!element.getName().equals(player.getName()))) {
+                element.setInPut(false);
+                player.setInPut(true);
+            } else {
+                System.out.println("Helaas! In de put. Je mag verder spelen als er een andere speler in komt");
+                player.setInPut(true);
+            }
+        }
+    }
+
     private static boolean currentPlaceAboveFinish(boolean endGame, int finish, Player player, int thrownNumber) {
         if (player.getCurrentPlace() >= finish) {
             if (player.getCurrentPlace() == finish) {
@@ -139,7 +162,7 @@ public class GanzenbordApplication {
                 int tooManyThrownEyes = player.getCurrentPlace() - finish;
                 player.setCurrentPlace(finish - tooManyThrownEyes);
                 System.out.println("Helaas! Dat is te ver. Je moet precies op " + finish + " eindigen om te winnen. Je staat nu weer op " + player.getCurrentPlace() + ".");
-                System.out.println("Gooi je dobbelsteen (g).");
+                endGame = false;
             }
         }
         return endGame;
@@ -147,6 +170,6 @@ public class GanzenbordApplication {
 
     private static int throwDice() {
         Random random = new Random();
-        return random.nextInt(6 - 1) + 1;
+        return random.nextInt(6) + 1;
     }
 }
